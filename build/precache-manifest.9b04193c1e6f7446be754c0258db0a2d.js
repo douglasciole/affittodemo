b15c7a921a0f54a057ad9994b9600129d73a3ba4
Douglas Ciole <douglasciole@gmail.com>
2020-05-19 15:29:16 -0700

    Site

---
+++ build/precache-manifest.9b04193c1e6f7446be754c0258db0a2d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0ce663f7aa2558ad67b13a372c8e78b",
+    "url": "/affittodemo/index.html"
+  },
+  {
+    "revision": "164065867afd04a1288b",
+    "url": "/affittodemo/static/css/main.1ecf01d5.chunk.css"
+  },
+  {
+    "revision": "1262f75c66672f51c779",
+    "url": "/affittodemo/static/js/2.e6a434a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/affittodemo/static/js/2.e6a434a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "164065867afd04a1288b",
+    "url": "/affittodemo/static/js/main.bfe164ff.chunk.js"
+  },
+  {
+    "revision": "40797b13156ad3d265b8",
+    "url": "/affittodemo/static/js/runtime-main.4cbce6e7.js"
+  },
+  {
+    "revision": "0759fc0bf0cabbc5449a0277a2e37891",
+    "url": "/affittodemo/static/media/AvenirLTStd-Book.0759fc0b.woff"
+  },
+  {
+    "revision": "9334084d0d28fc558b1056e3fd06c600",
+    "url": "/affittodemo/static/media/AvenirLTStd-Book.9334084d.woff2"
+  },
+  {
+    "revision": "6ec907c8fa59dd6c235e2dc0cfb7b9fe",
+    "url": "/affittodemo/static/media/AvenirLTStd-Medium.6ec907c8.woff"
+  },
+  {
+    "revision": "809636269cc737c26ca3a5a19421f8a6",
+    "url": "/affittodemo/static/media/AvenirLTStd-Medium.80963626.woff2"
+  },
+  {
+    "revision": "538baeddc2107cd44dec3d5008f7270b",
+    "url": "/affittodemo/static/media/AvenirLTStd-Roman.538baedd.woff2"
+  },
+  {
+    "revision": "57ab854152635af8ed06c276310e3e0c",
+    "url": "/affittodemo/static/media/AvenirLTStd-Roman.57ab8541.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/affittodemo/static/media/logo.5d5d9eef.svg"
+  }
+]);
